f3a0f122895a296cb2f2aae54be5938bd1c6d79e
Sha-Kumar <shashankneeded@gmail.com>
2023-11-30 13:44:03 +0000

    image code

--- main.py
+++ main.py
@@ -2,12 +2,42 @@ from typing import Union
 
 from fastapi import FastAPI
 
+
+# import cv2
+import base64
+
 app = FastAPI()
+# Initialize the webcam
+# cap = cv2.VideoCapture(0)
+
+def capture_image():
+
+    # Capture a frame from the webcam
+    # ret, frame = cap.read()
+
+    # Convert the image to base64
+    # _, img_encoded = cv2.imencode('.jpg', frame)
+    # img_base64 = base64.b64encode(img_encoded.tobytes()).decode('utf-8')
+
+    return img_base64
+
+
+# def capture():
+#     try:
+#         # Capture an image from the webcam
+#         image_base64 = capture_image()
+
+#         return jsonify({'image': image_base64})
+
+#     except Exception as e:
+#         return jsonify({'error': str(e)})
+
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+    # return capture()
 
 
 @app.get("/items/{item_id}")
