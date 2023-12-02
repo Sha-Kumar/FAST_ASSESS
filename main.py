588f1243e7064783068c4af4c88c005be82f02e8
Sha-Kumar <shashankneeded@gmail.com>
2023-12-02 07:17:47 +0000

    requirements added

--- main.py
+++ main.py
@@ -1,45 +1,82 @@
 from typing import Union
-
 from fastapi import FastAPI
 
-
-# import cv2
+import cv2
 import base64
+import time
+import os
 
 app = FastAPI()
-# Initialize the webcam
-# cap = cv2.VideoCapture(0)
 
-def capture_image():
-
-    # Capture a frame from the webcam
-    # ret, frame = cap.read()
-
-    # Convert the image to base64
-    # _, img_encoded = cv2.imencode('.jpg', frame)
-    # img_base64 = base64.b64encode(img_encoded.tobytes()).decode('utf-8')
-
-    return img_base64
+def time_calculator():
+    # Milliseconds Precision time data
+    return int(time.time() * 1000)
 
 
-# def capture():
-#     try:
-#         # Capture an image from the webcam
-#         image_base64 = capture_image()
+def image_capture(cap,timestamp,filePrefix):
 
-#         return jsonify({'image': image_base64})
+    #Initalization
+    currentFrame = 0
 
-#     except Exception as e:
-#         return jsonify({'error': str(e)})
+    # Run a loop to continuously capture and save images
+    while True:
+        # Read a frame from the webcam
+        ret, frame = cap.read()
 
+        # Check if the frame was read successfully
+        if not ret:
+            break
+
+        # Construct the filename with the timestamp
+        file_path = f"{filePrefix}/{timestamp}/image_{currentFrame}.jpg"
+
+        # Save the image to the specified file path
+        cv2.imwrite(file_path, frame)
+
+        # Increment loop parameter
+        currentFrame += 1
+
+        # # Display the captured frame (optional, you can remove this line if not needed)
+        # cv2.imshow('Captured Image', frame)
+
+        if currentFrame > 9:
+            break
+        
+    if currentFrame > 9:
+        return True
+    return False
+
+
+
+def capture():
+    # Initialize the webcam
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        return "Error: Could not open webcam."
+
+    folderName = "CAPTURES"
+
+    # Create a directory for storing captured images if it doesn't exist
+    os.makedirs(folderName, exist_ok=True)
+
+    # write images
+    success = image_capture(cap,time_calculator(),folderName)
+    if success == True:
+        return "Images Captured Successfully"
+
+    return "Something Went wrong"
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-    # return capture()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/test")
+def test():
+    return {"Hello": "Test API"}
+
+@app.get("/access")
+def access():
+    return capture()
